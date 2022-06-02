476dc7bad1e62455ec5a583b41a314180e40ca58
diego <89492190+diegopaterno@users.noreply.github.com>
2022-06-02 15:31:44 -0300

    Add files via upload

---
+++ bc_592066_ejercicio_03-Diego_Gauto-Diego_Paterno/js/main.js
@@ -0,0 +1,16 @@
+let nombre 
+            
+nombre = prompt('ingrese su nombre')
+console.log(nombre)
+
+let edad
+edad = prompt('ingrese su edad')
+console.log(edad)
+
+if (edad <=30){
+    console.log('sos un pibe')
+} else{
+    console.log('como va')
+}
+
+
